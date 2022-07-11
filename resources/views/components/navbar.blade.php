edc450090486b49001813f9e657ad6509e49650a
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-07-11 09:51:22 +0700

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -2,28 +2,25 @@
     <!-- LOGO -->
     <div class="navbar-brand-box">
         <!-- Dark Logo-->
-        <a href="index-2.html" class="logo logo-dark">
+        <a href="{{ url('/') }}" class="logo logo-dark">
             <span class="logo-sm">
-              <img src="{{asset('assets/images/logo-sm.png')}}" alt="" height="50" />
+                <img src="{{asset('assets/images/logo-sm.png')}}" alt="" height="50" />
             </span>
             <span class="logo-lg">
-              <img src="{{asset('assets/images/logo-dark.png')}}" alt="" height="50" />
+                <img src="{{asset('assets/images/logo-dark.png')}}" alt="" height="50" />
             </span>
         </a>
         <!-- Light Logo-->
-        <a href="index-2.html" class="logo logo-light">
+        <a href="{{ url('/') }}" class="logo logo-light">
             <span class="logo-sm">
-              <img src="{{asset('assets/images/logo-sm.png')}}" alt="" height="100" />
+                <img src="{{asset('assets/images/logo-sm.png')}}" alt="" height="100" />
             </span>
             <span class="logo-lg">
-              <img src="{{asset('assets/images/logo-light.png')}}" alt="" height="100" />
+                <img src="{{asset('assets/images/logo-light.png')}}" alt="" height="100" />
             </span>
         </a>
-        <button
-            type="button"
-            class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
-            id="vertical-hover"
-        >
+        <button type="button" class="btn btn-sm p-0 fs-20 header-item float-end btn-vertical-sm-hover"
+            id="vertical-hover">
             <i class="ri-record-circle-line"></i>
         </button>
     </div>
@@ -42,25 +39,15 @@
                 </li>
 
                 <li class="nav-item">
-                    <a
-                        class="nav-link menu-link"
-                        href="#sidebarApps"
-                        data-bs-toggle="collapse"
-                        role="button"
-                        aria-expanded="false"
-                        aria-controls="sidebarApps"
-                    >
+                    <a class="nav-link menu-link" href="#sidebarApps" data-bs-toggle="collapse" role="button"
+                        aria-expanded="false" aria-controls="sidebarApps">
                         <i class="ri-contacts-book-2-line"></i>
                         <span data-key="t-apps">Mahasiswa</span>
                     </a>
                     <div class="collapse menu-dropdown" id="sidebarApps">
                         <ul class="nav nav-sm flex-column">
                             <li class="nav-item">
-                                <a
-                                    href="{{route('students.index')}}"
-                                    class="nav-link"
-                                    data-key="t-chat"
-                                >
+                                <a href="{{route('students.index')}}" class="nav-link" data-key="t-chat">
                                     Semua Mahasiswa
                                 </a>
                             </li>
