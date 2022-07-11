2dcbe0818690af6b3b3d17ebf52e66d4775766c4
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-07-11 08:57:31 +0700

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -33,7 +33,7 @@
                 <div class="dropdown ms-sm-3 header-item topbar-user">
                     <button type="button" class="btn" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             <span class="d-flex align-items-center">
-              <img class="rounded-circle header-profile-user" src="{{asset('assets/images/users/avatar-3.jpg')}}" alt="Header Avatar" />
+              <img class="rounded-circle header-profile-user" src="{{asset('assets/images/users/avatar-2.jpg')}}" alt="Header Avatar" />
               <span class="text-start ms-xl-2">
                 <span class="d-none d-xl-inline-block ms-1 fw-medium user-name-text">{{auth()->user()->name}}</span>
                 <span class="d-none d-xl-block ms-1 fs-12 text-muted user-name-sub-text">Admin Staff</span>
@@ -42,7 +42,7 @@
                     </button>
                     <div class="dropdown-menu dropdown-menu-end">
                         <!-- item-->
-                        <h6 class="dropdown-header">Welcome Anna!</h6>
+                        <h6 class="dropdown-header">Welcome {{auth()->user()->name}}!</h6>
                         <a class="dropdown-item" href="{{route('staff.detail', auth()->id())}}">
                             <i class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i>
                             <span class="align-middle">Profile</span>
