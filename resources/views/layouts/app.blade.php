b70f4e1f1731f9fb35ae09ea18bf28d175b91e08
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-07-11 15:45:19 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,109 +1,105 @@
 <!DOCTYPE html>
-<html
-    lang="id"
-    data-layout="vertical"
-    data-topbar="light"
-    data-sidebar="light"
-    data-sidebar-size="lg"
-    data-sidebar-image="none"
->
+<html lang="id" data-layout="vertical" data-topbar="light" data-sidebar="light" data-sidebar-size="lg"
+    data-sidebar-image="none">
+
 <head>
-    <meta charset="utf-8"/>
-    <title>{{ ($breadcrumb = Breadcrumbs::current()) ? $breadcrumb->title : 'Fallback Title' }} | Platform Pendaftaran Proposal</title>
+    <meta charset="utf-8" />
+    <title>{{ ($breadcrumb = Breadcrumbs::current()) ? $breadcrumb->title : 'Fallback Title' }} | Platform Pendaftaran
+        Proposal</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description"/>
-    <meta content="Themesbrand" name="author"/>
+    <meta content="Platform Pendaftaran Unit Layanan Statistik" name="description" />
+    <meta content="Abdul Aziz" name="author" />
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- App favicon -->
     <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
 
     <!-- Bootstrap Css -->
-    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css" />
     <!-- Icons Css -->
-    <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
     <!-- App Css-->
-    <link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/css/app.min.css')}}" rel="stylesheet" type="text/css" />
     <!-- custom Css-->
-    <link href="{{asset('assets/css/custom.min.css')}}" rel="stylesheet" type="text/css"/>
+    <link href="{{asset('assets/css/custom.min.css')}}" rel="stylesheet" type="text/css" />
     @vite(['resources/js/app.js'])
-    <script src="https://cdn.tiny.cloud/1/3kubek8r1p1mz4kvit7hc1z2mxd8wgg551cbeu82qkmenprf/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="https://cdn.tiny.cloud/1/3kubek8r1p1mz4kvit7hc1z2mxd8wgg551cbeu82qkmenprf/tinymce/6/tinymce.min.js"
+        referrerpolicy="origin"></script>
 
 </head>
+
 <body>
-<!-- Begin page -->
-<div id="layout-wrapper">
-    <x-header/>
-    <!-- ========== App Menu ========== -->
-    <x-navbar/>
-    <!-- Left Sidebar End -->
-    <!-- Vertical Overlay-->
-    <div class="vertical-overlay"></div>
+    <!-- Begin page -->
+    <div id="layout-wrapper">
+        <x-header />
+        <!-- ========== App Menu ========== -->
+        <x-navbar />
+        <!-- Left Sidebar End -->
+        <!-- Vertical Overlay-->
+        <div class="vertical-overlay"></div>
 
-    <!-- ============================================================== -->
-    <!-- Start right Content here -->
-    <!-- ============================================================== -->
-    <div class="main-content">
-        <div class="page-content">
-            <div class="container-fluid">
-                <!-- start page title -->
-                {{Breadcrumbs::render()}}
-                <!-- end page title -->
+        <!-- ============================================================== -->
+        <!-- Start right Content here -->
+        <!-- ============================================================== -->
+        <div class="main-content">
+            <div class="page-content">
+                <div class="container-fluid">
+                    <!-- start page title -->
+                    {{Breadcrumbs::render()}}
+                    <!-- end page title -->
 
-                <div class="row">
-                    <div class="col">
-                        <div class="h-100">
-                            {{$slot}}
+                    <div class="row">
+                        <div class="col">
+                            <div class="h-100">
+                                {{$slot}}
+                            </div>
+                            <!-- end .h-100-->
                         </div>
-                        <!-- end .h-100-->
+                        <!-- end col -->
                     </div>
-                    <!-- end col -->
                 </div>
+                <!-- container-fluid -->
             </div>
-            <!-- container-fluid -->
+            <!-- End Page-content -->
+
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <script>
+                                document.write(new Date().getFullYear());
+                            </script>
+                            © Bimbingin.
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="text-sm-end d-none d-sm-block">
+                                Design & Develop by <a class="fw-bold" href="https://facebook.com/lazizdev">Abdul
+                                    Aziz</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
-        <!-- End Page-content -->
+        <!-- end main content-->
 
-        <footer class="footer">
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <script>
-                            document.write(new Date().getFullYear());
-                        </script>
-                        © Bimbingin.
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="text-sm-end d-none d-sm-block">
-                            Design & Develop by <a class="fw-bold" href="https://facebook.com/lazizdev">Abdul Aziz</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
+        <button onclick="topFunction()" class="btn btn-danger btn-icon" id="back-to-top">
+            <i class="ri-arrow-up-line"></i>
+        </button>
     </div>
-    <!-- end main content-->
-
-    <button
-        onclick="topFunction()"
-        class="btn btn-danger btn-icon"
-        id="back-to-top"
-    >
-        <i class="ri-arrow-up-line"></i>
-    </button>
-</div>
-<!-- END layout-wrapper -->
-<!-- JAVASCRIPT -->
-<script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
-<script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
-<script src="{{asset('assets/libs/feather-icons/feather.min.js')}}"></script>
-<script src="{{asset('assets/js/pages/plugins/lord-icon-2.1.0.js')}}"></script>
-<!-- App js -->
-<script src="{{asset('assets/js/app.js')}}"></script>
-@livewireScripts
-<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<x-livewire-alert::scripts />
-@stack('scripts')
+    <!-- END layout-wrapper -->
+    <!-- JAVASCRIPT -->
+    <script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{asset('assets/libs/simplebar/simplebar.min.js')}}"></script>
+    <script src="{{asset('assets/libs/node-waves/waves.min.js')}}"></script>
+    <script src="{{asset('assets/libs/feather-icons/feather.min.js')}}"></script>
+    <script src="{{asset('assets/js/pages/plugins/lord-icon-2.1.0.js')}}"></script>
+    <!-- App js -->
+    <script src="{{asset('assets/js/app.js')}}"></script>
+    @livewireScripts
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <x-livewire-alert::scripts />
+    @stack('scripts')
 </body>
+
 </html>
