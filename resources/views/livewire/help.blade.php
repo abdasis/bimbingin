cea073e0fa771ef288a3912ced12c633b2a7ee83
Abd. Asis <34927165+abdasis@users.noreply.github.com>
2022-07-12 20:31:40 +0700

    Update help.blade.php

--- resources/views/livewire/help.blade.php
+++ resources/views/livewire/help.blade.php
@@ -11,7 +11,7 @@
                         <p>Jika anda masih merasa kesulitan dalam melakukan pendaftaran silahkan ikut langkah berikut</p>
                         <img class="img-fluid my-3 border-light rounded border" src="{{asset('assets/images/bantuan1-01.png')}}" alt="">
                         <img class="img-fluid my-3 border-light rounded border" src="{{asset('assets/images/Bantuan2-01.png')}}" alt="">
-                        <img class="img-fluid my-3 border-light rounded border" src="{{asset('assets/images/bantuan3-01.png')}}" alt="">
+                        <img class="img-fluid my-3 border-light rounded border" src="{{asset('assets/images/Bantuan3-01.png')}}" alt="">
                     </div>
                 </div>
             </div>
